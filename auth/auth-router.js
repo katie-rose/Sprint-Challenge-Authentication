38c316d20f2109b00087f29fcdc026bde08975c3
Katie Rose <katiefelten@gmail.com>
2019-08-24 12:07:57 -0400

    :zap: added users model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const usersDb = require("../users/users-model");
+
+// get users
+router.get('/users', (req, res) => {
+	usersDb.find()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(error => {
+			res.status(500).json(error)
+		});
+});
+
+router.post("/register", (req, res) => {
   // implement registration
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
