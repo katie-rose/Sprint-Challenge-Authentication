a08e00258ab7a23213f3e4bd3321efa225b535a8
Katie Rose <katiefelten@gmail.com>
2019-08-24 12:22:44 -0400

    :zap: SWEET JESUS WE HAVE A LOGIN WORKING AND A TOKEN BEING SENT :sparkles:

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const jwt = require("jsonwebtoken");
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -14,5 +15,24 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.get("/token", (req, res) => {
+  const role = "admin";
+  const payload = {
+    subject: "me",
+    role
+  };
+  const secret = "is it secret, is it safe?";
+  const options = {
+    expiresIn: "8h"
+  };
+  const token = jwt.sign(payload, secret, options);
+
+  res.status(200).json({ role: role, token });
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,55 +1,70 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const usersDb = require("../users/users-model");
+const secrets = require("../config/secrets");
 
-// get users
-router.get('/users', (req, res) => {
-	usersDb.find()
-		.then(users => {
-			res.status(200).json(users);
-		})
-		.catch(error => {
-			res.status(500).json(error)
-		});
+router.get("/users", (req, res) => {
+  usersDb
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post("/register", (req, res) => {
-  // implement registration
-  	let user = req.body;
-    const hash = bcrypt.hashSync(user.password);
-    user.password = hash;
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
 
-    usersDb
-      .add(user)
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+  usersDb
+    .add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post("/login", (req, res) => {
-  // implement login
-  	let { username, password } = req.body;
+  let { username, password } = req.body;
 
-    usersDb
-      .findByUsername(username)
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
+  usersDb
+    .findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwt(user);
 
-          res.status(200).json({
-            message: `Welcome ${user.username}`
-          });
-        } else {
-          res.status(401).json({ message: "Invalid login credentials" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: "Error communicating with database" });
-      });
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid login credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error communicating with database" });
+    });
 });
+
+function getJwt(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username
+	}
+
+	const options = {
+		expiresIn: '8h'
+	}
+
+	return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 
 module.exports = router;
